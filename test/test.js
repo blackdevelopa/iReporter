53a488e3270f6ff39d00a573e32fddca77bb814c
blackdevelopa <blackdevelopa@gmail.com>
2018-11-27 13:44:22 +0100

    created test for home route

--- test/test.js
+++ test/test.js
@@ -1,18 +1,20 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+const expect = require('chai').expect;
 
 chai.use(chaiHttp);
 
-const server = require('../server/app');
+const app = require('../server/app');
 const should = chai.should();
 
 //Test Home Route
 describe('/GET home route', () => {
   it('should GET home page', (done) => {
-    chai.request(server)
+    chai.request(app)
       .get('/')
       .end((err, res) => {
 				res.should.have.status(200);
+				res.should.be.json;
         done();
       });
   });
