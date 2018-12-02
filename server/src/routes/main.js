e3bca337cc0a262b3337f611ed1a2b64fd7aac35
blackdevelopa <blackdevelopa@gmail.com>
2018-12-02 20:36:56 +0100

    created get redflag and get interventions

--- server/src/routes/main.js
+++ server/src/routes/main.js
@@ -3,6 +3,9 @@ import IncidentController from '../controllers/incident';
 
 const router = express.Router();
 
-router.get('/incidents', IncidentController.getAll);
+router.get('/incidents', IncidentController.getAllIncidents);
+router.get('/redflags', IncidentController.getAllRedflags);
+router.get('/interventions', IncidentController.getAllInterventions);
+
 
 export default router;
