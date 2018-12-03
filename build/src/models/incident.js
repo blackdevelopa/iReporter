12e1e23a85249e43ba8fb29d31848cad652cc009
blackdevelopa <blackdevelopa@gmail.com>
2018-12-03 01:41:14 +0100

    added the build directory

---
+++ build/src/models/incident.js
@@ -0,0 +1,31 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+// Default incident details
+
+var IncidentModel = [{
+  id: 1,
+  createdOn: new Date().toLocaleString(),
+  createdBy: 1,
+  type: 'redflag',
+  location: '6.4732° N, 3.2947° E',
+  status: 'draft',
+  images: ['images', 'images'],
+  videos: ['videos', 'videos'],
+  comment: 'Police officers are exhorting drivers'
+}, {
+  id: 2,
+  createdOn: new Date().toLocaleString(),
+  createdBy: 2,
+  type: 'intervention',
+  location: '6.5946° N, 3.3405° E',
+  status: 'draft',
+  images: ['images', 'images'],
+  videos: ['videos', 'videos'],
+  comment: 'The pedestrian bridge is broken'
+}];
+
+exports.default = IncidentModel;
+//# sourceMappingURL=incident.js.map
