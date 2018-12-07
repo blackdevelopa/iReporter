8b00f7f67943a97c7f6ca051e4731dd1959e1c4f
blackdevelopa <blackdevelopa@gmail.com>
2018-12-07 04:13:58 +0100

    bug(userHome): Implemented LFA feedback - added footer across all pages fixed the issue with modal popping up in homepage [Delivers: #162437674]

--- UI/user/js/user-home.js
+++ UI/user/js/user-home.js
@@ -2,8 +2,6 @@ const modal = document.querySelector('#modal');
 const modalOverlay = document.querySelector('#modal-overlay');
 const closeBtn = document.querySelector('#closebtn');
 const openBtn = document.querySelector('#openbtn');
-const inputField = document.querySelectorAll('.cord');
-const displayMessage = document.querySelector('.error');
 
 openBtn.addEventListener('click', () => {
   modal.classList.toggle('closed');
@@ -15,10 +13,7 @@ closeBtn.addEventListener('click', () => {
   modalOverlay.classList.toggle('closed');
 });
 
-const validateInput = () => {
-  displayMessage = "";
-  const num = inputField.value;
-    if(!Number(num)) {
-      return displayMessage.innerHTML = `<p>Please enter numbers</p>`
-    }
-};
+closeModal = () => {
+  modal.classList.toggle('closed');
+  modalOverlay.classList.toggle('closed');
+}
