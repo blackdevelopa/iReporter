4eb690f359cb343ccea07e08f3d2623efd3e3a56
blackdevelopa <blackdevelopa@gmail.com>
2018-12-07 05:00:39 +0100

    fix travis

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import IncidentController from '../controllers/Controller';
+import IncidentController from '../controllers/incidentController';
 
 const router = express.Router();
 
