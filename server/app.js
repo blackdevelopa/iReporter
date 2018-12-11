7b1a7d954b2e0392f7f567473a5557956bd79990
blackdevelopa <blackdevelopa@gmail.com>
2018-12-11 13:52:19 +0100

    chore(test): added test for entry point for database

--- server/app.js
+++ server/app.js
@@ -3,21 +3,29 @@ import bodyParser from 'body-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
 import 'babel-polyfill';
-// import inMemory from './inMemory/src/controllers/incidentController';
-// import dataBase from './dataBase/controllers/incidentController';
-import indexRouter from './dataBase/router/indexRouter';
 
 const app = express();
 
 dotenv.config();
-// const Incident = process.env.TYPE === 'db' ? dataBase : inMemory;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 
 const port = process.env.PORT || 3000;
 
-app.use('/', indexRouter);
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: 'This is the iReporter application'
+  });
+});
+
+app.get('/*', (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: 'This is an invalid route. Please see proper documentation'
+  })
+})
 
 
 app.listen(port);
--- server/dataBase/router/indexRouter.js
+++ server/dataBase/router/indexRouter.js
@@ -1,14 +1,1 @@
-import { Router } from 'express';
-import incidentRouter from './incidentRouter';
-const indexRouter = Router();
 
-indexRouter.get('/', (req, res) => {
-  return res.status(200).json({
-    status: 200,
-    message: 'Welcome to iReporter'
-  });
-});
-
-indexRouter.use('/api/v1/', incidentRouter);
-
-export default indexRouter;
