87c6127780fefdd7009dbba7cfaf1bd6d40579f7
Black Developa <blackdevelopa@gmail.com>
2019-01-28 11:08:44 +0100

    bug(server): refactoring codes     - written swagger api documentation

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import swaggerUI from 'swagger-ui-express';
+import doc from '../doc.json';
 import dotenv from 'dotenv';
 import 'babel-polyfill';
 import router from './router/index';
@@ -13,6 +15,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 
 const port = process.env.PORT || 3000;
+
+// serve swagger doc
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(doc));
 
 app.use(router);
 
